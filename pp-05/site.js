9f21391d50c9bfffb5eb0c27e86efb46e6ddd998
allisonej <eunji3620@gmail.com>
2017-12-04 00:05:10 -0600

    commit pp-05

--- pp-05/site.js
+++ pp-05/site.js
@@ -2,3 +2,14 @@ function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+
+/*button Audio*/
+doorbellring.addEventListener("click", function() {
+  ringDoorbell();
+});
+
+$(document).on("keypress", function(press) {
+  if (press.key ==="d") {
+    ringDoorbell();
+  }
+});
